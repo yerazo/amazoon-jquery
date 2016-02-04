5d9337916939998c4172aee4ed7cbb36190f1825
Tyler Lambe <tyler@batchacademy.com>
2016-02-04 04:08:18 -0600

    Initial commit

---
+++ assets/js/cart.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    var products = [
+      {
+        name: "Amazon Fire TV",
+        manufacturer: "Amazoon",
+        imageSrc: "assets/images/product01.jpg",
+        description: "Eligible for free shipping and returns",
+        price: 89.99
+      },
+      {
+        name: "Amazon Fire",
+        manufacturer: "Amazoon",
+        imageSrc: "assets/images/product02.jpg",
+        description: "Eligible for free shipping and returns",
+        price: 69.00
+      },
+      {
+        name: "Amazon Echo",
+        manufacturer: "Amazoon",
+        imageSrc: "assets/images/product03.jpg",
+        description: "Eligible for free shipping and returns",
+        price: 109.99
+      }
+    ];
+
+    // Get started here!
+
+  });
---
+++ assets/js/checkout.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  // Get started here!
+});
